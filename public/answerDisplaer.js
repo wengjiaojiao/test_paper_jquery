f42ed37fd3897c16beecc467c84bf6deb40b9406
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-08 18:38:48 +0800

    use jquery to modifiy

--- public/answerDisplaer.js
+++ public/answerDisplaer.js
@@ -1,28 +1,30 @@
-[].forEach.call(document.getElementsByName("radio1"), function(item, i) {
+[].forEach.call($(":input[name=radio1]"), function(item, i) {
   item.onclick = function () {
-      document.getElementById("answer1").value = item.value;
+      //document.getElementById("answer1").value = item.value;
+      $(":input[id=answer1]").prop("value", item.value);
   };
 });
-[].forEach.call(document.getElementsByName("radio2"), function(item, i) {
+[].forEach.call($(":input[name=radio2]"), function(item, i) {
   item.onclick = function () {
-      document.getElementById("answer2").value = item.value;
+      //document.getElementById("answer2").value = item.value;
+      $(":input[id=answer1]").prop("value", item.value);    
   };
 });
-[].forEach.call(document.getElementsByName("checkbox1"), function(item, i) {
+[].forEach.call($(":input[name=checkbox1]"), function(item, i) {
   item.onclick = function () {
       var ans = '';
       $(':input[name=checkbox1]').each(function () {
         ans += this.checked ? this.value : '';
       })
-      document.getElementById("answer3").value = ans;
+      $(":input[id=answer3]").prop("value", ans);
   };
 });
-[].forEach.call(document.getElementsByName("checkbox2"), function(item, i) {
+[].forEach.call($(":input[name=checkbox2]"), function(item, i) {
   item.onclick = function () {
     var ans = '';
     $(':input[name=checkbox2]').each(function () {
       ans += this.checked ? this.value : '';
     })
-    document.getElementById("answer4").value = ans;
+    $(":input[id=answer4]").prop("value", ans);
   };
 });
